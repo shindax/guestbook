f4dd967e000337a0a99add5c9a0c13f3b09d8004
shindax <shindax@mail.ru>
2022-10-06 08:23:59 +0700

    Slug added. createdAt autoinsert added. Event listeners added

--- src/Controller/Admin/CommentCrudController.php
+++ src/Controller/Admin/CommentCrudController.php
@@ -50,10 +50,10 @@ class CommentCrudController extends AbstractCrudController
         'years' => range(date('Y'), date('Y') + 5),
         'widget' => 'single_text',]);
         
-        if(Crud::PAGE_EDIT === $pageName) {
-            yield $createdAt->setFormTypeOption('disabled', true);
-            } else {
-                yield $createdAt;
-            }
+        // if(Crud::PAGE_EDIT === $pageName) {
+        //     yield $createdAt->setFormTypeOption('disabled', true);
+        //     } else {
+        //         yield $createdAt;
+        //     }
     }
 }
